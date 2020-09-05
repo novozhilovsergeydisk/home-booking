0d3b6dbab23571cabe025bc13d1da98b4051f9ac
Novozhilov Sergey <novozhilovsergey@MacBook-Pro-Novozhilov.local>
2020-09-05 14:41:37 +0300

    изменено: README.md

---
+++ git-init.sh
@@ -0,0 +1,7 @@
+!# /bin/bash
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/novozhilovsergeydisk/home-booking.git .
+git push -u origin master
